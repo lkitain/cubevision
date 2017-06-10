461f138a8fc62e1ebdfcdbd2319ec7571f882bb7
Ian Hook <ianhook@gmail.com>
2017-06-10 10:43:21 -0700

    not sure I can lint this

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
@@ -9,10 +10,10 @@ app.use(express.static(__dirname + '/public'));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
-app.get('/', function(request, response) {
+app.get('/', (request, response) => {
   response.render('pages/index');
 });
 
-app.listen(app.get('port'), function() {
+app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
 });
