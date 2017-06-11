77e92e88aefc0dddaa27d85c240dc3c087f69940
Ian Hook <ianhook@gmail.com>
2017-06-11 15:12:47 -0700

    db does not work locally

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const path = require('path');
+const pg = require('pg');
 
 const app = express();
 
@@ -8,9 +9,25 @@ app.set('port', (process.env.PORT || 5000));
 app.use(express.static(path.resolve(__dirname, './public')));
 
 app.get('/', (request, response) => {
-  response.render('public/index.html');
+    response.render('public/index.html');
+});
+
+app.get('/cube/:cubeId', (request, response) => {
+    const cubeId = request.params.cubeId;
+    pg.connect(process.env.DATABASE_URL, (err, client, done) => {
+        console.log(err);
+        client.query(`select * from cube_cards join cubes using(cube_id) join cards using (card_id) where cube_id = ${cubeId};`, (err, result) => {
+            done();
+            if (err) {
+                console.error(err);
+                response.send(`Error ${err}`);
+            } else {
+                response.send(result.rows);
+            }
+        });
+    });
 });
 
 app.listen(app.get('port'), () => {
-  console.log('Node app is running on port', app.get('port'));
+    console.log('Node app is running on port', app.get('port'));
 });
