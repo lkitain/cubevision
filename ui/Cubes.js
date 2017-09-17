e704f648b9db1c93720be85415a64c8e828d8b3d
Ian Hook <ianhook@gmail.com>
2017-09-17 12:23:53 -0700

    link to cube

--- ui/Cubes.js
+++ ui/Cubes.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 const Cubes = ({ cubes }) => (
   <div>
     <h2>Cubes</h2>
     <ul>
       {Object.keys(cubes).map(cubeId => (
-        <li key={cubeId}>{cubes[cubeId].name}</li>
+        <li key={cubeId}>
+          <Link to={`/cube/${cubeId}`}>
+            {cubes[cubeId].name}
+          </Link>
+        </li>
       ))}
     </ul>
   </div>
