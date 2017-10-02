3c09605178d502640325a44c7c7c4ca809be866c
Ian Hook <ianhook@gmail.com>
2017-10-01 17:02:08 -0700

    better ux

--- ui/Sorter.js
+++ ui/Sorter.js
@@ -1,50 +1,70 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
+import Radium from 'radium';
 
 const Sorter = ({ onChange }) => {
     return (
         <div>
-            <label htmlFor="standard">Exclude Standard</label>
-            <input
-                id="standard"
-                type="checkbox"
-                onChange={e => onChange('standard', e.target.checked)}
-            />
-            <label htmlFor="current">Only Current Cube</label>
-            <input
-                id="current"
-                type="checkbox"
-                onChange={e => onChange('current', e.target.checked)}
-            />
-            <label htmlFor="color">Color</label>
-            <input
-                id="color"
-                type="radio"
-                name="sort"
-                onChange={e => onChange(e.target.name, e.target.id)}
-            />
-            <label htmlFor="name">Name</label>
-            <input
-                id="name"
-                type="radio"
-                name="sort"
-                onChange={e => onChange(e.target.name, e.target.id)}
-            />
-            <label htmlFor="types">Types</label>
-            <input
-                id="types"
-                type="radio"
-                name="sort"
-                onChange={e => onChange(e.target.name, e.target.id)}
-            />
-            <label htmlFor="age">Age(?)</label>
-            <input
-                id="age"
-                type="radio"
-                name="sort"
-                onChange={e => onChange(e.target.name, e.target.id)}
-            />
+            <div style={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                '@media (min-width: 500px)': {
+                    justifyContent: 'start',
+                },
+            }}
+            >
+                <label htmlFor="standard">Exclude Standard</label>
+                <input
+                    id="standard"
+                    type="checkbox"
+                    onChange={e => onChange('standard', e.target.checked)}
+                />
+                <label htmlFor="current">Only Current Cube</label>
+                <input
+                    id="current"
+                    type="checkbox"
+                    onChange={e => onChange('current', e.target.checked)}
+                />
+            </div>
+            <div style={{
+                marginTop: 8,
+                display: 'flex',
+                justifyContent: 'space-between',
+                '@media (min-width: 500px)': {
+                    justifyContent: 'start',
+                },
+            }}
+            >
+                <label htmlFor="color">Color</label>
+                <input
+                    id="color"
+                    type="radio"
+                    name="sort"
+                    onChange={e => onChange(e.target.name, e.target.id)}
+                />
+                <label htmlFor="name">Name</label>
+                <input
+                    id="name"
+                    type="radio"
+                    name="sort"
+                    onChange={e => onChange(e.target.name, e.target.id)}
+                />
+                <label htmlFor="types">Types</label>
+                <input
+                    id="types"
+                    type="radio"
+                    name="sort"
+                    onChange={e => onChange(e.target.name, e.target.id)}
+                />
+                <label htmlFor="age">Age(?)</label>
+                <input
+                    id="age"
+                    type="radio"
+                    name="sort"
+                    onChange={e => onChange(e.target.name, e.target.id)}
+                />
+            </div>
         </div>
     );
 };
@@ -59,6 +79,6 @@ const mapDispatchToProps = dispatch => ({
     }),
 });
 
-const ConnectedSorter = connect(mapStateToProps, mapDispatchToProps)(Sorter);
+const ConnectedSorter = connect(mapStateToProps, mapDispatchToProps)(Radium(Sorter));
 
 export default ConnectedSorter;
