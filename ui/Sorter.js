be629bc008f8714fa28eb829f6400ff37a0458c4
lkitain <lkitain@gmail.com>
2023-08-02 17:11:26 -0700

    Update Sorter.js

--- ui/Sorter.js
+++ ui/Sorter.js
@@ -6,61 +6,79 @@ import Radium from 'radium';
 const Sorter = ({ onChange, isCurrentCube }) => (
     <div>
         <div style={{
-            display: 'flex',
+            display: 'flex', // Use flexbox to align elements horizontally
+            alignItems: 'center', // Align items vertically centered
             justifyContent: 'space-between',
             '@media (min-width: 500px)': {
                 justifyContent: 'start',
             },
         }}
         >
-            <label htmlFor="standard">
-                Exclude Standard
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="standard"
                     type="checkbox"
                     onChange={(e) => onChange('standard', e.target.checked)}
                 />
-            </label>
-            <label htmlFor="current">
-                { `Only ${isCurrentCube ? 'Our' : 'Current'} Cube` }
+                <label htmlFor="standard" style={{ marginLeft: '4px' }}>
+                    Exclude Standard
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="current"
                     type="checkbox"
                     onChange={(e) => onChange('current', e.target.checked)}
                 />
-            </label>
-            <label htmlFor="excludeCurrent">
-                { `Exclude ${isCurrentCube ? 'Our' : 'Current'} Cube` }
+                <label htmlFor="current" style={{ marginLeft: '4px' }}>
+                    { `Only ${isCurrentCube ? 'Our' : 'Current'} Cube` }
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="excludeCurrent"
                     type="checkbox"
                     onChange={(e) => onChange('excludeCurrent', e.target.checked)}
                 />
-            </label>
-            <label htmlFor="reserved">
-                Only Reserved
+                <label htmlFor="excludeCurrent" style={{ marginLeft: '4px' }}>
+                    { `Exclude ${isCurrentCube ? 'Our' : 'Current'} Cube` }
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="reserved"
                     type="checkbox"
                     onChange={(e) => onChange('reserved', e.target.checked)}
                 />
-            </label>
-            <label htmlFor="missing">
-                Missing
+                <label htmlFor="reserved" style={{ marginLeft: '4px' }}>
+                    Only Reserved
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="missing"
                     type="checkbox"
                     onChange={(e) => onChange('missing', e.target.checked)}
                 />
-            </label>
-            <label htmlFor="replacements">
-                Suggest Replacements
+                <label htmlFor="missing" style={{ marginLeft: '4px' }}>
+                    Missing
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="replacements"
                     type="checkbox"
                     onChange={(e) => onChange('replacements', e.target.checked)}
                 />
-            </label>
+                <label htmlFor="replacements" style={{ marginLeft: '4px' }}>
+                    Suggest Replacements
+                </label>
+            </div>
         </div>
         <div style={{
             marginTop: 8,
@@ -71,63 +89,81 @@ const Sorter = ({ onChange, isCurrentCube }) => (
             },
         }}
         >
-            <label htmlFor="color">
-                Color
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="color"
                     type="radio"
                     name="sort"
                     onChange={(e) => onChange(e.target.name, e.target.id)}
                 />
-            </label>
-            <label htmlFor="name">
-                Name
+                <label htmlFor="color" style={{ marginLeft: '4px' }}>
+                    Color
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="name"
                     type="radio"
                     name="sort"
                     onChange={(e) => onChange(e.target.name, e.target.id)}
                 />
-            </label>
-            <label htmlFor="types">
-                Types
+                <label htmlFor="name" style={{ marginLeft: '4px' }}>
+                    Name
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="types"
                     type="radio"
                     name="sort"
                     onChange={(e) => onChange(e.target.name, e.target.id)}
                 />
-            </label>
-            <label htmlFor="age">
-                Age(?)
+                <label htmlFor="types" style={{ marginLeft: '4px' }}>
+                    Types
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="age"
                     type="radio"
                     name="sort"
                     onChange={(e) => onChange(e.target.name, e.target.id)}
                 />
-            </label>
-            <label htmlFor="cost">
-                Mana Cost
+                <label htmlFor="age" style={{ marginLeft: '4px' }}>
+                    Age(?)
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="cost"
                     type="radio"
                     name="sort"
                     onChange={(e) => onChange(e.target.name, e.target.id)}
                 />
-            </label>
-            <label htmlFor="lastCube">
-                Last Cube
+                <label htmlFor="cost" style={{ marginLeft: '4px' }}>
+                    Mana Cost
+                </label>
+            </div>
+
+            <div style={{ display: 'flex', alignItems: 'center', marginRight: '8px' }}>
                 <input
                     id="lastCube"
                     type="radio"
                     name="sort"
                     onChange={(e) => onChange(e.target.name, e.target.id)}
                 />
-            </label>
+                <label htmlFor="lastCube" style={{ marginLeft: '4px' }}>
+                    Last Cube
+                </label>
+            </div>
         </div>
     </div>
 );
+
 
 Sorter.propTypes = {
     onChange: PropTypes.func.isRequired,
